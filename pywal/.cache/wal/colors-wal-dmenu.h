493ba75cc20a096254a7bf2e870631d0a8e29b71
sire <sire.saini26@gmail.com>
2025-12-04 21:23:41 +0530

    stop tracking pywal cache

--- pywal/.cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#c1c0c0", "#090404" },
-	[SchemeSel] = { "#c1c0c0", "#575757" },
-	[SchemeOut] = { "#c1c0c0", "#A7A7A7" },
-};
--- pywal/.cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#090404";
-static const char* selfgcolor   = "#c1c0c0";
-static const char* normbgcolor  = "#686868";
-static const char* normfgcolor  = "#c1c0c0";
-static const char* urgbgcolor   = "#575757";
-static const char* urgfgcolor   = "#c1c0c0";
