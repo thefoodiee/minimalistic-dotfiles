7fe8da3130f2b21be956c14b47ff11526f950f1f
sire <sire.saini27@gmail.com>
2026-04-10 22:17:43 +0530

    switched to tui bluetooth

--- waybar/.config/waybar/scripts/bluetooth.sh
+++ waybar/.config/waybar/scripts/bluetooth.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# Get the address of the nm-connection-editor window
-# Requires 'jq' - install with 'sudo apt install jq' or 'sudo pacman -S jq'
-WINDOW_ADDRESS=$(hyprctl clients -j | jq -r '.[] | select(.class == "blueman-manager") | .address')
+# Name of the Kitty window class or title to detect
+WINDOW_CLASS="kitty"
+WINDOW_TITLE="bluetuith"
 
+# Check if a Kitty window running nmtui already exists
+WINDOW_ADDRESS=$(hyprctl clients -j | jq -r \
+    ".[] | select(.class == \"$WINDOW_CLASS\" and .title == \"$WINDOW_TITLE\") | .address")
+  
 if [ -n "$WINDOW_ADDRESS" ]; then
-    # If the window exists, close it
+    # Close the window if found
     hyprctl dispatch killwindow "address:$WINDOW_ADDRESS"
 else
-    # If the window does not exist, open nm-connection-editor
-    blueman-manager & disown
+    # Launch Kitty running nmtui
+    kitty --title "$WINDOW_TITLE" bluetuith & disown
 fi
