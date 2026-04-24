1caf369fbeecfdbaa1b5b0645118de7a2e5b1620
sire <sire.saini27@gmail.com>
2026-04-24 18:07:23 +0530

    added styling

--- hypr/.config/hypr/scripts/songdetail.sh
+++ hypr/.config/hypr/scripts/songdetail.sh
@@ -4,5 +4,5 @@ if playerctl -p spotify status &>/dev/null; then
     song_info=$(playerctl -p spotify metadata --format "{{title}} - {{artist}}")
     echo "<span weight='heavy'>  $song_info</span>"
 else
-    echo "<span alpha='0.6'>no media</span>"
+    echo "<span weight='heavy'>no media</span>"
 fi
