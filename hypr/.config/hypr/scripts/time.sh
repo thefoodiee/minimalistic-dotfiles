ab47f79b60ccba3dbbec27271491031845169e66
sire <sire.saini27@gmail.com>
2026-05-13 18:23:12 +0530

    changed time display config

---
+++ hypr/.config/hypr/scripts/time.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+hour=$(date +%H)
+
+if [ "$hour" -ge 5 ] && [ "$hour" -lt 12 ]; then
+    icon="󰖨" # morning
+elif [ "$hour" -ge 12 ] && [ "$hour" -lt 17 ]; then
+    icon="󰖙" # afternoon
+elif [ "$hour" -ge 17 ] && [ "$hour" -lt 21 ]; then
+    icon="󰖛" # evening
+else
+    icon="󰖔" # night
+fi
+
+echo "<span weight='heavy'>$(date +'%I:%M') $icon </span>"
