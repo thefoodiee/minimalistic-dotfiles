20c17603f8002c79cd645c82ae0e763bbced6289
sire <sire.saini27@gmail.com>
2026-05-17 11:22:57 +0530

    added music gestures

--- hypr/.config/hypr/modules/input.lua
+++ hypr/.config/hypr/modules/input.lua
@@ -7,7 +7,7 @@ hl.config({
 		kb_layout = "us",
 		kb_variant = "",
 		kb_model = "",
-		kb_options = "caps:escape",
+		kb_options = "caps:swapescape",
 		kb_rules = "",
 
 		follow_mouse = 1,
@@ -28,3 +28,34 @@ hl.gesture({
 	action = "workspace",
 })
 
+hl.gesture({
+	fingers = 4,
+	direction = "up",
+	action = function()
+		hl.exec_cmd("swayosd-client --output-volume raise")
+	end,
+})
+
+hl.gesture({
+	fingers = 4,
+	direction = "down",
+	action = function()
+		hl.exec_cmd("swayosd-client --output-volume lower")
+	end,
+})
+
+hl.gesture({
+	fingers = 4,
+	direction = "left",
+	action = function()
+		hl.exec_cmd("playerctl -p spotify previous")
+	end,
+})
+
+hl.gesture({
+	fingers = 4,
+	direction = "right",
+	action = function()
+		hl.exec_cmd("playerctl -p spotify next")
+	end,
+})
