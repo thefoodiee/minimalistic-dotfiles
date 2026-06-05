928a361796cbb0da7c2e2dc24337519e7eb86b3e
sire <sire.saini27@gmail.com>
2026-06-05 16:20:44 +0530

    folding experimentation

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -6,3 +6,10 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+--
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
+vim.opt.foldenable = true
