b17fca9bcf28723ebe1d3c0fdfad16176a3332a4
sire <sire.saini27@gmail.com>
2026-07-29 10:37:53 +0530

    swayosd fixes

--- hypr/.config/hypr/modules/keybindings/user-system.lua
+++ hypr/.config/hypr/modules/keybindings/user-system.lua
@@ -13,6 +13,7 @@ local screenshot = 'hyprshot -m region -o "' .. os.getenv("HOME") .. '/Pictures/
 -- audio
 hl.bind("F12", hl.dsp.exec_cmd("swayosd-client  --player spotify --playerctl play-pause"))
 hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("swayosd-client  --player spotify --playerctl play-pause"))
+hl.bind("XF86AudioPause", hl.dsp.exec_cmd("swayosd-client --player spotify --playerctl play-pause"))
 hl.bind("XF86AudioNext", hl.dsp.exec_cmd("swayosd-client  --player spotify --playerctl next"))
 hl.bind(mainMod .. " + CTRL + V", hl.dsp.exec_cmd("[workspace special:kitty-sinkswitch; float; move 1430 44; size 480 160] kitty --class kitty-scratch -e ~/.config/hypr/scripts/sinkswitch.sh -exclude 46"))
 
