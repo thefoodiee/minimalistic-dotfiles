9cd40ab2b8db9f80eab903413476a987289d8c4e
sire <sire.saini27@gmail.com>
2026-08-07 11:53:15 +0530

    move to pywal

---
+++ wal/.config/wal/templates/colors-hyprland.lua
@@ -0,0 +1,22 @@
+return {
+    background = "{background}",
+    foreground = "{foreground}",
+
+    black   = "{color0}",
+    red     = "{color1}",
+    green   = "{color2}",
+    yellow  = "{color3}",
+    blue    = "{color4}",
+    magenta = "{color5}",
+    cyan    = "{color6}",
+    white   = "{color7}",
+
+    bright_black   = "{color8}",
+    bright_red     = "{color9}",
+    bright_green   = "{color10}",
+    bright_yellow  = "{color11}",
+    bright_blue    = "{color12}",
+    bright_magenta = "{color13}",
+    bright_cyan    = "{color14}",
+    bright_white   = "{color15}",
+}
